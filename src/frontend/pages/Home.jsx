51d730394b0ade4b3034fea7a69b286801bdb18f
Suresh-Ben <sureshbennabatthula@gmail.com>
2023-01-06 22:17:01 +0530

    Project init
    React and hardhat initiated

---
+++ src/frontend/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Upload from '../components/Home/Upload';
+import Display from '../components/Home/Display';
+import Shared from '../components/Home/Shared';
+
+function Home() {
+    return (
+        <h1>Home Page</h1>
+    );
+}
+
+export default Home;
