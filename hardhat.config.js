5dc1aa333f98fca196d4ab11b271ec94d4b08708
Suresh-Ben <sureshbennabatthula@gmail.com>
2023-01-07 15:32:22 +0530

    requires downloaded

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.17",
-  paths: {
-    artifacts: "./src/backend/artifacts",
-    sources: "./src/backend/contracts",
-    cache: "./src/backend/cache",
-    tests: "./src/backend/test"
-  }
+    solidity: "0.8.17",
+    paths: {
+        artifacts: "./src/backend/artifacts",
+        sources: "./src/backend/contracts",
+        cache: "./src/backend/cache",
+        tests: "./src/backend/test"
+    }
 };
